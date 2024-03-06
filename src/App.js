4aba67bcaf54b6dfd4c1d9ba7b91c29bb1ec1679
unknown <poojapmp912@gmail.com>
2024-03-06 09:23:26 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import RegistrationForm from './components/RegistrationForm';
-import {Container} from "@mui/material/";
+import { Container, AppBar,Box } from "@mui/material/";
+import logo from './assets/logo.png';
 
 const App = () => {
   return (
-    <Container
-      sx={{
-        display: 'block',
-        mx:'auto',
-        my:'auto'
-      }}
-      maxWidth='sm'
-    >
-<RegistrationForm />
-   </Container>
-      
+    <Fragment>
+      <AppBar position="static">
+            <Box
+              component="img"
+              sx={{ width: '100%',
+              fit:'fill',
+              height:'auto' }}
+              p={0}
+              alt="Logo"
+              src={logo}
+            />
+      </AppBar>
+      <Container
+        sx={{
+          display: 'block',
+          mx: 'auto',
+          my: 2
+        }}
+        maxWidth='sm'
+      >
+        <RegistrationForm />
+      </Container>
+    </Fragment>
+
+
   );
 };
 
 export default App;
+
+
+
